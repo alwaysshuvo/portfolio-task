e8fca27a18d7fc0345a2e5d2b98e0b3cbb27ee18
alwaysshuvo <mds126258@gmail.com>
2026-01-28 10:49:55 +0600

    skills section added

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -2,133 +2,144 @@ import Image from "next/image";
 
 export default function Resume() {
   return (
-    <section className="w-full bg-[#171B1A] py-24 px-16">
-      <h2 className="text-center text-3xl font-bold text-white mb-20">
+    <section className="w-full bg-[#171B1A] py-24">
+      <h2 className="text-center text-[48px] font-bold text-white mb-20">
         Resume
       </h2>
 
-      <div className="max-w-[900px] mx-auto relative pl-20">
-        {/* Vertical Line */}
-        <div className="absolute left-8 top-0 h-full w-[2px] bg-[#2a2f2e]"></div>
+      {/* Left Empty Content */}
+      <div className="flex">
+        {/* Left empty space = hero image width */}
+        <div className="w-[525px]"></div>
 
-        {/* ================= Education ================= */}
-        <div className="mb-20">
-          <div className="flex items-center gap-3 mb-10">
-            <Image
-              src="/icons/education.png"
-              alt="education"
-              width={22}
-              height={22}
-            />
-            <h3 className="text-[#F5BD4D] font-semibold text-lg">
-              Education
-            </h3>
-          </div>
+        {/* Right content */}
+        <div className="flex-1 pr-16">
+          {/*Education */}
+          <div className="mb-24 relative pl-16">
+            {/* Stick */}
+            <div className="absolute left-6 top-12 h-[85%] w-[2px] bg-[#2a2f2e]"></div>
 
-          {/* Item 1 */}
-          <div className="relative mb-12">
-            <span className="absolute left-[-38px] top-2 w-3 h-3 bg-[#F5BD4D] rounded-full"></span>
+            {/* Title */}
+            <div className="flex items-center gap-3 mb-10">
+              <Image
+                src="/icons/education.png"
+                alt="edu"
+                width={32}
+                height={32}
+              />
+              <h3 className="text-white font-semibold text-[24px]">
+                Education
+              </h3>
+            </div>
 
-            <span className="inline-block mb-3 px-3 py-1 text-xs rounded-full bg-[#1f1f1f] text-gray-300">
-              2016–2020
-            </span>
+            {/* Item 1 */}
+            <div className="relative mb-12">
+              <span className="absolute left-[-28px] top-2 w-3 h-3 bg-[#F5BD4D] rounded-full"></span>
 
-            <div className="bg-[#1a1e1d] rounded-xl p-6 border border-[#2a2f2e]">
-              <h4 className="text-[#F5BD4D] font-semibold">
-                Harvard University
-              </h4>
-              <p className="text-sm text-gray-400 mb-3">
-                B.A. and M.S. in Computer Science
-              </p>
-              <p className="text-sm text-gray-500 leading-relaxed">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Ab aut commodi earum eligendi ipsum, laborum maiores mollitia,
-                optio quam quis rerum totam voluptas.
-              </p>
+              <span className="inline-block mb-3 px-3 py-1 text-[14px] rounded-[16px] border border-white text-text">
+                2016–2020
+              </span>
+
+              <div className="bg-[#1a1e1d] rounded-xl p-6 border border-[#2a2f2e]">
+                <h4 className="text-[18px] gradient-text font-bold">
+                  Harvard University
+                </h4>
+                <p className="text-sm text-[##8F8F8F] mb-3">
+                  B.A. and M.S. in Computer Science
+                </p>
+                <div className="w-full border-b border-[1px] border-[#222222] mb-3"></div>
+
+                <p className="text-[16px] text-white leading-relaxed">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab
+                  aut commodi earum eligendi ipsum, laborum maiores mollitia,
+                  optio quam quis rerum totam voluptas.
+                </p>
+              </div>
+            </div>
+
+            {/* Item 2 */}
+            <div className="relative">
+              <span className="absolute left-[-28px] top-2 w-3 h-3 gradient-bg rounded-full"></span>
+
+              <span className="inline-block mb-3 px-3 py-1 text-[14px] rounded-[16px] border border-white text-text">
+                2014–2016
+              </span>
+
+              <div className="bg-[#1a1e1d] rounded-xl p-6 border border-[#2a2f2e]">
+                <h4 className="text-[18px] gradient-text font-bold">
+                  Helwan University
+                </h4>
+                <p className="text-sm text-[##8F8F8F] mb-3">Bachelor Degree</p>
+
+                <div className="w-full border-b border-[1px] border-[#222222] mb-3"></div>
+
+                <p className="text-[16px] text-white leading-relaxed">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab
+                  aut commodi earum eligendi ipsum, laborum maiores mollitia,
+                  optio quam quis rerum totam voluptas.
+                </p>
+              </div>
             </div>
           </div>
 
-          {/* Item 2 */}
-          <div className="relative">
-            <span className="absolute left-[-38px] top-2 w-3 h-3 bg-[#F5BD4D] rounded-full"></span>
+          {/*  Work History */}
+          <div className="relative pl-16">
+            {/* Stick */}
+            <div className="absolute left-6 top-12 h-[85%] w-[2px] bg-[#2a2f2e]"></div>
 
-            <span className="inline-block mb-3 px-3 py-1 text-xs rounded-full bg-[#1f1f1f] text-gray-300">
-              2014–2016
-            </span>
-
-            <div className="bg-[#1a1e1d] rounded-xl p-6 border border-[#2a2f2e]">
-              <h4 className="text-[#F5BD4D] font-semibold">
-                Helwan University
-              </h4>
-              <p className="text-sm text-gray-400 mb-3">
-                Bachelor Degree
-              </p>
-              <p className="text-sm text-gray-500 leading-relaxed">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Ab aut commodi earum eligendi ipsum, laborum maiores mollitia,
-                optio quam quis rerum totam voluptas.
-              </p>
+            {/* Title */}
+            <div className="flex items-center gap-3 mb-10">
+              <Image
+                src="/icons/work-history.png"
+                alt="work"
+                width={32}
+                height={32}
+              />
+              <h3 className="text-white font-semibold text-[24px]">
+                Work History
+              </h3>
             </div>
-          </div>
-        </div>
 
-        {/* ================= Work History ================= */}
-        <div>
-          <div className="flex items-center gap-3 mb-10">
-            <Image
-              src="/icons/work-history.png"
-              alt="work-history"
-              width={22}
-              height={22}
-            />
-            <h3 className="text-[#F5BD4D] font-semibold text-lg">
-              Work History
-            </h3>
-          </div>
+            {/* Item 1 */}
+            <div className="relative mb-12">
+              <span className="absolute left-[-28px] top-2 w-3 h-3 gradient-bg rounded-full"></span>
 
-          {/* Item 1 */}
-          <div className="relative mb-12">
-            <span className="absolute left-[-38px] top-2 w-3 h-3 bg-[#F5BD4D] rounded-full"></span>
+              <span className="inline-block mb-3 px-3 py-1 text-[14px] rounded-[16px] border border-white text-text">
+                2019–2023
+              </span>
 
-            <span className="inline-block mb-3 px-3 py-1 text-xs rounded-full bg-[#1f1f1f] text-gray-300">
-              2019–2023
-            </span>
-
-            <div className="bg-[#1a1e1d] rounded-xl p-6 border border-[#2a2f2e]">
-              <h4 className="text-[#F5BD4D] font-semibold">
-                UI/UX Design
-              </h4>
-              <p className="text-sm text-gray-400 mb-3">
-                Web Site Design
-              </p>
-              <p className="text-sm text-gray-500 leading-relaxed">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Ab aut commodi earum eligendi ipsum, laborum maiores mollitia,
-                optio quam quis rerum totam voluptas.
-              </p>
+              <div className="bg-[#1a1e1d] rounded-xl p-6 border border-[#2a2f2e]">
+                <h4 className="text-[18px] gradient-text font-bold">UI/UX Design</h4>
+                <p className="text-sm text-[##8F8F8F] mb-3">Web Site Design</p>
+                <div className="w-full border-b border-[1px] border-[#222222] mb-3"></div>
+                <p className="text-[16px] text-white leading-relaxed">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ab
+                  aut commodi earum eligendi ipsum, laborum maiores mollitia,
+                  optio quam quis rerum totam voluptas.
+                </p>
+              </div>
             </div>
-          </div>
 
-          {/* Item 2 */}
-          <div className="relative">
-            <span className="absolute left-[-38px] top-2 w-3 h-3 bg-[#F5BD4D] rounded-full"></span>
+            {/* Item 2 */}
+            <div className="relative">
+              <span className="absolute left-[-28px] top-2 w-3 h-3 gradient-bg rounded-full"></span>
 
-            <span className="inline-block mb-3 px-3 py-1 text-xs rounded-full bg-[#1f1f1f] text-gray-300">
-              2017–2019
-            </span>
+              <span className="inline-block mb-3 px-3 py-1 text-[14px] rounded-[16px] border border-white text-text">
+                2017–2019
+              </span>
 
-            <div className="bg-[#1a1e1d] rounded-xl p-6 border border-[#2a2f2e]">
-              <h4 className="text-[#F5BD4D] font-semibold">
-                Junior Web Designer
-              </h4>
-              <p className="text-sm text-gray-400 mb-3">
-                Product Design
-              </p>
-              <p className="text-sm text-gray-500 leading-relaxed">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Ab aut commodi earum eligendi ipsum, laborum maiores mollitia,
-                optio quam quis rerum totam voluptas.
-              </p>
+              <div className="bg-[#1a1e1d] rounded-xl p-6 border border-[#2a2f2e]">
+                <h4 className="text-[18px] gradient-text font-bold">
+                  Junior Web Designer
+                </h4>
+                <p className="text-[##8F8F8F] mb-3">Product Design</p>
+                <div className="w-full border-b border-[1px] border-[#222222] mb-3"></div>
+                <p className="text-[16px] text-white leading-relaxed">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ab
+                  aut commodi earum eligendi ipsum, laborum maiores mollitia,
+                  optio quam quis rerum totam voluptas.
+                </p>
+              </div>
             </div>
           </div>
         </div>
